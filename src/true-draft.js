8f4e460b86f5359982df3d6865004495e0d5e341
yangabrielreis <yangabrielreis@gmail.com>
2024-12-10 00:14:04 -0300

    draft v2

--- src/true-draft.js
+++ src/true-draft.js
@@ -10,29 +10,53 @@ function importAll(r) {
 }
 
 const heroIcons = importAll(require.context('./images/heroIcons', false, /\.(png|jpe?g|svg)$/));
+const blankImage = require('./images/Blank.png'); // Import the blank image
 
 function TrueDraft() {
+    const [selectedHeroesTop, setSelectedHeroesTop] = useState([]);
     const [selectedHeroesLeft, setSelectedHeroesLeft] = useState([]);
     const [selectedHeroesRight, setSelectedHeroesRight] = useState([]);
     const [isAmberTurn, setIsAmberTurn] = useState(true);
+    const [availableHeroIcons, setAvailableHeroIcons] = useState(heroIcons);
 
     const handleIconClick = (iconName) => {
-        const heroIcon = heroIcons[iconName];
-        if (isAmberTurn) {
+        const heroIcon = availableHeroIcons[iconName];
+        if (heroIcon === blankImage) return; // Do nothing if the blank image is selected
+
+        if (selectedHeroesTop.length < 4) {
+            setSelectedHeroesTop([...selectedHeroesTop, heroIcon]);
+            const newAvailableHeroIcons = { ...availableHeroIcons };
+            newAvailableHeroIcons[iconName] = blankImage;
+            setAvailableHeroIcons(newAvailableHeroIcons);
+        } else if (isAmberTurn) {
             if (selectedHeroesLeft.length < 6) {
+                const newAvailableHeroIcons = { ...availableHeroIcons };
+                newAvailableHeroIcons[iconName] = blankImage;
+                setAvailableHeroIcons(newAvailableHeroIcons);
                 setSelectedHeroesLeft([...selectedHeroesLeft, heroIcon]);
                 setIsAmberTurn(!isAmberTurn);
             }
         } else {
             if (selectedHeroesRight.length < 6) {
+                const newAvailableHeroIcons = { ...availableHeroIcons };
+                newAvailableHeroIcons[iconName] = blankImage;
+                setAvailableHeroIcons(newAvailableHeroIcons);
                 setSelectedHeroesRight([...selectedHeroesRight, heroIcon]);
                 setIsAmberTurn(!isAmberTurn);
             }
         }
     };
+    
 
     return (
         <div className="App-header">
+            <div className="selected-heroes-box-top">
+                {selectedHeroesTop.map((heroIcon, index) => (
+                    <div key={index} className="selected-hero">
+                        <img src={heroIcon} alt={`Hero ${index}`} />
+                    </div>
+                ))}
+            </div>
             <div className="selected-heroes-box-left">
                 {selectedHeroesLeft.map((heroIcon, index) => (
                     <div key={index} className="selected-hero">
@@ -48,13 +72,13 @@ function TrueDraft() {
                 ))}
             </div>
             <div className="icon-box">
-                {Object.keys(heroIcons).map((iconName, index) => (
+                {Object.keys(availableHeroIcons).map((iconName, index) => (
                     <button 
                         key={index} 
                         className="icon-button" 
                         onClick={() => handleIconClick(iconName)}
                     >
-                        <img src={heroIcons[iconName]} alt={iconName} />
+                        <img src={availableHeroIcons[iconName]} alt={iconName} />
                     </button>
                 ))}
             </div>
